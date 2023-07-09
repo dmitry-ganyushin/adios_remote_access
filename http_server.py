358a55264387ca1f5cd7f20cb5bcc5a4e3caf969
Dmitry Ganyushin <ganyushin@gmail.com>
2023-07-09 18:13:58 -0400

    WIP.Added a ability to send remote file size back

--- http_server.py
+++ http_server.py
@@ -18,26 +18,38 @@ class ADIOS_HTTP_Request(BaseHTTPRequestHandler):
         print(self.path)
         filepath = self.path
         remote_file = sftp.file(filepath, 'r')
-        ranges = self.headers["Range"].split("=")[1].split("-")
-        start_byte = int(ranges[0])
-        end_byte = int(ranges[1])
-        remote_file.seek(start_byte)  # Move the file pointer to the desired starting byte
-        data = remote_file.read(end_byte - start_byte + 1)
-        print(data)
+        header = self.headers["Range"]
+        if (header):
+            ranges = header.split("=")[1].split("-")
+            start_byte = int(ranges[0])
+            end_byte = int(ranges[1])
+            remote_file.seek(start_byte)  # Move the file pointer to the desired starting byte
+            data = remote_file.read(end_byte - start_byte + 1)
+            print(data)
 
-        """send data back"""
-        self.wfile.write(data)
+            """send data back"""
+            self.wfile.write(data)
 
-REMOTE_HOST = input("hostname: ")
+        header = self.headers["Content-Length"]
+        if (header):
+            data = remote_file.stat()
+            print(data.st_size)
+
+            """send data back"""
+            self.wfile.write(bytes(str(int(data.st_size)),"utf-8)"))
+
+# REMOTE_HOST = input("hostname: ")
+REMOTE_HOST = "localhost"
 transport = paramiko.Transport((REMOTE_HOST, 22))
 # Auth username,password = "bar","foo"
 
-user = input("Username: ")
-try:
-    password = getpass.getpass()
-except Exception as error:
-    print('ERROR', error)
-
+# user = input("Username: ")
+# try:
+#     password = getpass.getpass()
+# except Exception as error:
+#     print('ERROR', error)
+user = "ganyush"
+password = "Ugmetpamcirj21*("
 transport.connect(None, user, password)
 # Go!
 sftp = paramiko.SFTPClient.from_transport(transport)
